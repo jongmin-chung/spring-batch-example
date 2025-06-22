c192c969b805c4349ed019bce2b319dd20aa93ee
jongmin-chung <chungjm0711@gmail.com>
2025-06-22 23:18:19 +0900

    init project

---
+++ src/test/java/io/github/jongminchung/springbatchexample/SpringBatchExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jongminchung.springbatchexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
